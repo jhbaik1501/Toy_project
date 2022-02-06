98acae626332ea2778be687ac774b67989c848fd
jonghyun <jhbaik1501@gmail.com>
2022-02-07 04:44:56 +0900

    3주차

--- Toy_project/src/main/java/com/example/Toy_project/controller/controllerTest2.java
+++ Toy_project/src/main/java/com/example/Toy_project/controller/controllerTest2.java
@@ -1,6 +1,7 @@
 package com.example.Toy_project.controller;
 
 import com.example.Toy_project.DataRepositoryEx;
+import com.example.Toy_project.Environ_Repository;
 import com.example.Toy_project.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -13,17 +14,17 @@ import java.util.Map;
 @RestController
 public class controllerTest2 {
 
-    private final DataRepositoryEx dataRepositoryEx;
+    private final Environ_Repository environ_repository;
 
     @Autowired
-    public controllerTest2(DataRepositoryEx dataRepositoryEx) {
-        this.dataRepositoryEx = dataRepositoryEx;
+    public controllerTest2(Environ_Repository environ_repository) {
+        this.environ_repository = environ_repository;
     }
 
 
     @GetMapping("/home2")
     public String getHome(Model model){
-        Environment environment = dataRepositoryEx.getNowEnvironment();
+        Environment environment = environ_repository.getNowEnvironment();
         model.addAttribute("environment", environment);
         return "시간 : " + environment.getTime() + " 습도 : " + environment.getHumidity() + "% 조도 : " +
                 environment.getIlluminace() + "% 온도 : " + environment.getTemperature() + "도 ";
